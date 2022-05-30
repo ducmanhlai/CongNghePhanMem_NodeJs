cd8ba7ccdda0d9f0150c1ee5f1e7b46449b791b1
ducmanhlai <ducmanhlai22@gmail.com>
2022-05-31 00:32:33 +0700

    fix

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ app.use(cors())
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.listen(3000,()=>{console.log("đang chạy")});
 app.post("/api/baitheonam",jsonParser,data.getyear);
-app.post("/api/baitheothang",jsonParser,data.getmonth)
+app.post("/api/baitheothang",jsonParser,data.getmonth);
+app.get("/api/countND",data.countTk);
+app.get("/api/countBVCD",data.countBv)
--- service/getdata.js
+++ service/getdata.js
@@ -5,7 +5,7 @@ class getdata{
     async getyear(req,res) {
         var result;
         try {
-            await sql.connect('Server=localhost;Database=CNPM;User Id=ducmanh;Password=1234;Encrypt=true;TrustServerCertificate=True')
+            await sql.connect('Server=localhost;Database=CNPM;User Id=ducmanh;Password=1234;Encrypt=false;TrustServerCertificate=True')
             let year= req.body.data
             result  = await sql.query`exec [dbo].[sp_ThongKeTheoNam] @nam =${year}`
         } catch (err) {
@@ -16,10 +16,32 @@ class getdata{
     async getmonth(req,res){
         var result;
         try {
-            await sql.connect('Server=localhost;Database=CNPM;User Id=ducmanh;Password=1234;Encrypt=true;TrustServerCertificate=True')
+            await sql.connect('Server=localhost;Database=CNPM;User Id=ducmanh;Password=1234;Encrypt=false;TrustServerCertificate=True')
             let year= req.body.year
             let month= req.body.month
             result  = await sql.query`exec [dbo].[sp_ThongKeTheoThang] @nam =${year}, @thang=${month}`
+        } catch (err) {
+            console.log(err)
+        }
+        res.send(result.recordset);
+    }
+    async countTk(req,res){
+        var result;
+        try {
+            await sql.connect('Server=localhost;Database=CNPM;User Id=ducmanh;Password=1234;Encrypt=false;TrustServerCertificate=True')
+            
+            result  = await sql.query`select count(*) as tk from taikhoan`
+        } catch (err) {
+            console.log(err)
+        }
+        res.send(result.recordset);
+    }
+    async countBv(req,res){
+        var result;
+        try {
+            await sql.connect('Server=localhost;Database=CNPM;User Id=ducmanh;Password=1234;Encrypt=false;TrustServerCertificate=True')
+            
+            result  = await sql.query`select count(*) as bv from baiviet where tinhtrang=0`
         } catch (err) {
             console.log(err)
         }
